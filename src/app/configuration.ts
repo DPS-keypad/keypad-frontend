ee92bfbec71e5a6f6093ebcb30ac7cb9e34fc85e
GledianMeta <g.meta@studenti.poliba.it>
2024-07-26 11:30:19 +0200

    examples

---
+++ src/app/configuration.ts
@@ -0,0 +1,5 @@
+// Indirizzo del Backend
+export class Backend {
+  public static url: string = "http://localhost:8000";
+}
+
--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Backend } from './src/app/configuration.ts';
 
+// Add the following import statement
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +14,7 @@ export class ConfigService {
   configHidden = this.hidden.asObservable();
   private selectedButtonId: number = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   toggleConfigHidden() {
     this.hidden.next(!this.hidden.value);
@@ -25,4 +29,44 @@ export class ConfigService {
     // chiamata a backend per settare questa azione al button selezionato
   }
 
+
+  /*// Metodo per il signup
+  public signup(user: any) {
+    return this.http.post(Backend.url + '/users/', user);
+  }
+
+  // Metodo per il login
+  public login(loginDetails: any) {
+    return this.http.post(Backend.url + '/authenticate/login', loginDetails)
+  }
+
+  // Get api list
+  public get_api_list() {
+    return this.http.post(Backend.url + '/api')
+  }
+
+  // Metodo di per ottenere i dati dell'utente
+  public getUser(): Observable<any> {
+    return this.http.get(Backend.url + '/users/singleuser/' + localStorage.getItem('id'));
+  }
+
+  // Metodo per eliminare l'account
+  public deleteUser(): Observable<any> {
+    return this.http.delete(Backend.url + '/users/delete/' + localStorage.getItem('id'));
+  }
+
+  // Metodo per fare update user
+  public updateUser(user) {
+    return this.http.put(Backend.url + '/users/update/' + localStorage.getItem('id'), user);
+  }
+
+  // Metodo di per ottenere il ranking
+  public getRanking(): Observable<any> {
+    return this.http.get(Backend.url + '/ranking');
+  }*/
+
+
 }
+
+
+
