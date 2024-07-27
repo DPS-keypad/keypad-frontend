d938a9d862c6702b8a0e6b6f66ceb622047735a0
salvatoregesualdo <salvigesualdo@libero.it>
2024-07-27 12:40:27 +0200

    correct fetch api and view

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -9,6 +9,8 @@ import { ConfigService } from '../service/config.service';
 export class ConfigComponent implements OnInit {
 
   configHidden = true;
+  keys = { "keyboard": [{ "description": "Cut", "id": "keyboard_cut", "key": "ctrl+x" }, { "description": "Copy", "id": "keyboard_copy", "key": "ctrl+c" }, { "description": "Paste", "id": "keyboard_paste", "key": "ctrl+v" }], "spotify": [{ "description": "Play/Pause", "id": "spotify_play" }, { "description": "Next", "id": "spotify_next" }, { "description": "Previous", "id": "spotify_previous" }] }
+  pots = [];
 
   constructor(
     private configService: ConfigService
@@ -18,9 +20,20 @@ export class ConfigComponent implements OnInit {
     this.configService.configHidden.subscribe((value) => {
       this.configHidden = value;
     });
+    // fetch the list of APIs
+    this.configService.getApiList().subscribe((data) => {
+      this.keys = data.keys;
+      this.pots = data.pots;
+      console.log(this.keys);
+      console.log(this.pots);
+    });
   }
 
   setAction(action: string) {
     this.configService.setAction(action);
+  }
+
+  getImage(key: Object) {
+    return "assets/images/icons/spotify.png"
   }
 }
--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -48,39 +48,4 @@ export class ConfigService {
       }),
     });
   }
-
-  /* // Metodo per il signup
-  public signup(user: any) {
-    return this.http.post(Backend.url + '/users/', user);
-  }
-
-  // Metodo per il login
-  public login(loginDetails: any) {
-    return this.http.post(Backend.url + '/authenticate/login', loginDetails)
-  }
-
-  // Get api list
-  public get_api_list() {
-    return this.http.post(Backend.url + '/api')
-  }
-
-  // Metodo di per ottenere i dati dell'utente
-  public getUser(): Observable<any> {
-    return this.http.get(Backend.url + '/users/singleuser/' + localStorage.getItem('id'));
-  }
-
-  // Metodo per eliminare l'account
-  public deleteUser(): Observable<any> {
-    return this.http.delete(Backend.url + '/users/delete/' + localStorage.getItem('id'));
-  }
-
-  // Metodo per fare update user
-  public updateUser(user) {
-    return this.http.put(Backend.url + '/users/update/' + localStorage.getItem('id'), user);
-  }
-
-  // Metodo di per ottenere il ranking
-  public getRanking(): Observable<any> {
-    return this.http.get(Backend.url + '/ranking');
-  } */
 }
