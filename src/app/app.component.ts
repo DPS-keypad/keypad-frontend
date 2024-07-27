8e9e2fc76a49245e6943f4db518d94b1642ed238
GledianMeta <g.meta@studenti.poliba.it>
2024-07-27 15:09:52 +0200

    spotify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConfigService } from './service/config.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dps-deck-app';
+
+
+  constructor(
+    private configService: ConfigService
+  ) { }
+
+  loginSpotify() {
+    this.configService.loginSpotify();
+  }
+
 }
--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -12,8 +12,7 @@ export class ConfigService {
   configHidden = this.hidden.asObservable();
   private selectedButtonId: number = 0;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   toggleConfigHidden() {
     this.hidden.next(!this.hidden.value);
@@ -24,7 +23,7 @@ export class ConfigService {
   }
 
   setAction(action: string) {
-    let key = "key" + this.selectedButtonId
+    let key = 'key' + this.selectedButtonId;
     console.log('Action: ' + action + ' Button ID: ' + key);
     const payload = {
       key: key,
@@ -32,13 +31,15 @@ export class ConfigService {
     };
     console.log(payload);
     console.log(Backend.url + '/set_api');
-    this.http.post(Backend.url + '/set_api', payload, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-    }).subscribe((data) => {
-      console.log(data);
-    });
+    this.http
+      .post(Backend.url + '/set_api', payload, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+      })
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
 
   getApiList(): Observable<any> {
@@ -49,38 +50,18 @@ export class ConfigService {
     });
   }
 
-  /* // Metodo per il signup
-  public signup(user: any) {
-    return this.http.post(Backend.url + '/users/', user);
+  loginSpotify(){
+
+    this.http.get(Backend.url + '/login', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    }).subscribe((data) => {
+      console.log(data);
+    });
+
+
   }
 
-  // Metodo per il login
-  public login(loginDetails: any) {
-    return this.http.post(Backend.url + '/authenticate/login', loginDetails)
-  }
 
-  // Get api list
-  public get_api_list() {
-    return this.http.post(Backend.url + '/api')
-  }
-
-  // Metodo di per ottenere i dati dell'utente
-  public getUser(): Observable<any> {
-    return this.http.get(Backend.url + '/users/singleuser/' + localStorage.getItem('id'));
-  }
-
-  // Metodo per eliminare l'account
-  public deleteUser(): Observable<any> {
-    return this.http.delete(Backend.url + '/users/delete/' + localStorage.getItem('id'));
-  }
-
-  // Metodo per fare update user
-  public updateUser(user) {
-    return this.http.put(Backend.url + '/users/update/' + localStorage.getItem('id'), user);
-  }
-
-  // Metodo di per ottenere il ranking
-  public getRanking(): Observable<any> {
-    return this.http.get(Backend.url + '/ranking');
-  } */
 }
